8271dd9cc1452be3b5e339b4da70a904d9f6b53b
randula78 <yrandula92@gmail.com>
2025-03-13 12:48:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/9k8C0T00/9376.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "Hello Im alive now",
 };
