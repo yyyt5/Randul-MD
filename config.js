26ebb028fb901d9c54e038b6e3deb50e778544bb
yyyt5 <yrandula0@gmail.com>
2025-03-13 18:14:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~We5FjCJC#n0kiQqNKaPben3ee5EVsYcvglaRhN3wkb4zVvi6cuFI",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/S4xNGNpD/629.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "*⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇️𝐇𝐞𝐲 𝐈'𝐦 💃Randul 𝐌𝐃 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : TDD",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
